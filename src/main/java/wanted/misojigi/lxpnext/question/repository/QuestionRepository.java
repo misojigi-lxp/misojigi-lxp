471d03161e3f3e73b807a0e4cdfe88c6d068b940
sohee <sohee5997@naver.com>
2026-06-23 10:35:28 +0900

    chore: @Reposiotry 어노테이션 추가

--- src/main/java/wanted/misojigi/lxpnext/question/repository/QuestionRepository.java
+++ src/main/java/wanted/misojigi/lxpnext/question/repository/QuestionRepository.java
@@ -2,9 +2,11 @@ package wanted.misojigi.lxpnext.question.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import wanted.misojigi.lxpnext.question.domain.Question;
 import wanted.misojigi.lxpnext.question.domain.QuestionStatus;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findByLectureIdAndStatus(Long lectureId, QuestionStatus status);
